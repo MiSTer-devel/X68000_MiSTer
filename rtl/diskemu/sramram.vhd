1ab9ee6a7f41ed9969497487ad9c786993491b41
Wark <wark@free.fr>
2021-05-16 15:37:56 +0200

    fix path in sramram.vhd - need to test

--- rtl/diskemu/sramram.vhd
+++ rtl/diskemu/sramram.vhd
@@ -75,7 +75,7 @@ BEGIN
 		clock_enable_output_a => "BYPASS",
 		clock_enable_output_b => "BYPASS",
 		indata_reg_b => "CLOCK0",
-		init_file => "../../../diskemu_mister/sramdefault.hex",
+		init_file => "rtl/diskemu/sramdefault.hex",
 		init_file_layout => "PORT_A",
 		intended_device_family => "Cyclone V",
 		lpm_type => "altsyncram",
@@ -150,7 +150,7 @@ END SYN;
 -- Retrieval info: PRIVATE: MAXIMUM_DEPTH NUMERIC "0"
 -- Retrieval info: PRIVATE: MEMSIZE NUMERIC "131072"
 -- Retrieval info: PRIVATE: MEM_IN_BITS NUMERIC "0"
--- Retrieval info: PRIVATE: MIFfilename STRING "../../../diskemu_mister/sramdefault.hex"
+-- Retrieval info: PRIVATE: MIFfilename STRING "rtl/diskemu/sramdefault.hex"
 -- Retrieval info: PRIVATE: OPERATION_MODE NUMERIC "3"
 -- Retrieval info: PRIVATE: OUTDATA_ACLR_B NUMERIC "0"
 -- Retrieval info: PRIVATE: OUTDATA_REG_B NUMERIC "0"
@@ -185,7 +185,7 @@ END SYN;
 -- Retrieval info: CONSTANT: CLOCK_ENABLE_OUTPUT_A STRING "BYPASS"
 -- Retrieval info: CONSTANT: CLOCK_ENABLE_OUTPUT_B STRING "BYPASS"
 -- Retrieval info: CONSTANT: INDATA_REG_B STRING "CLOCK0"
--- Retrieval info: CONSTANT: INIT_FILE STRING "../../../diskemu_mister/sramdefault.hex"
+-- Retrieval info: CONSTANT: INIT_FILE STRING "rtl/diskemu/sramdefault.hex"
 -- Retrieval info: CONSTANT: INIT_FILE_LAYOUT STRING "PORT_A"
 -- Retrieval info: CONSTANT: INTENDED_DEVICE_FAMILY STRING "Cyclone V"
 -- Retrieval info: CONSTANT: LPM_TYPE STRING "altsyncram"
